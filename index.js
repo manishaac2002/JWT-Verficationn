db587466ddea72f4827168673573416d0bd61dd6
sachinabs <sachinabs.js@gmail.com>
2023-03-16 14:45:32 +0530

    test - jsonWebToken generated and token is verified.

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-import express, { request, response } from "express";
+import express from "express"
 import jwt from "jsonwebtoken"
 
 const app = express()
 
-
 app.get('/api', (request, response) => {
     response.json({
         message: "Hey I'm there ! Welcome to this API service"
+    })
+})
+app.post('/api/posts', verifyToken, (request, response) => {
+    
+    console.log(request.token);
+
+    jwt.verify(request.token, 'secretkey', (error, authData) => {
+        
+        if (error) {
+            console.log(error);
+            response.sendStatus(403)
+        } else {
+            response.json({
+                message: "Posts created...!",
+                authData,
+            })
+        }
     })
 })
 
@@ -16,13 +32,30 @@ app.post('/api/login', (request, response) => {
         username: "Manisha",
         email: 'manisha@gmail.com'
     }
-    
-    jwt.sign({user : user}, "secretkey",(error,token) =>{
-        response.json({
-            token,
-        })
-    })          
+
+    jwt.sign({ user: user }, "secretkey", (error, token) => {
+        console.log("Token generated here.... ");
+        console.log(token);
+        response.json({token})
+    })
 })
+
+function verifyToken(request, response, next) {
+    console.log("Validate the token here.. ");
+    console.log(request.headers['authorization']);
+    const bearerHeader = request.headers['authorization']
+    console.log(typeof bearerHeader);
+    if (typeof bearerHeader != "undefined") {
+        console.log("------------------------------------------");
+        const bearerToken = bearerHeader.split(" ")[1]
+        console.log(bearerToken);
+        request.token = bearerToken
+        next()
+    } else {
+    
+        response.sendStatus(403) //forbidden
+    }
+}
 
 
 app.listen(3000, (request, response) => {
